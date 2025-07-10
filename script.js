2828eb8985e96c810d1090d9788ae8858cb37118
Tony Norris <anthonylnorris@hotmail.com>
2025-07-10 18:53:19 -0400

    Tic tac toe

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
+let currentTurn = 'X';
 
+function handleCellClick(cellId) {
+  const img = document.getElementById(cellId);
+  if (img.src.includes("SquareBlank.png")) {
+    img.src = currentTurn === 'X' ? "pics/SquareX.png" : "pics/SquareO.png";
+    currentTurn = currentTurn === 'X' ? 'O' : 'X';
+  }
+}
+
+const ImageIds = [
+  "left1", "left2", "left3",
+  "center1", "center2", "center3",
+  "right1", "right2", "right3"
+];
+
+ImageIds.forEach(id => {
+  document.getElementById(id).addEventListener("click", () => handleCellClick(id));
+});
